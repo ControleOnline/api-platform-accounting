82455ebef1515df012a3a03b4c4cc3e0e0ef2f7c
Luiz Kim <luiz.kim@controleonline.com>
2026-08-31 16:58:21 -0300

    fix(cte): add infQ to infCarga for CTe schema

--- src/Service/Cte/CteXmlBuilder.php
+++ src/Service/Cte/CteXmlBuilder.php
@@ -85,7 +85,7 @@ class CteXmlBuilder
             . $dest
             . sprintf('<vPrest><vTPrest>%s</vTPrest><vRec>%s</vRec></vPrest>', $vTPrest, $vTPrest)
             . '<imp><ICMS><ICMS00><CST>00</CST><vBC>0.00</vBC><pICMS>0.00</pICMS><vICMS>0.00</vICMS></ICMS00></ICMS></imp>'
-            . '<infCTeNorm><infCarga><vCarga>' . $vTPrest . '</vCarga><proPred>MERCADORIA</proPred></infCarga>'
+            . '<infCTeNorm><infCarga><vCarga>' . $vTPrest . '</vCarga><proPred>MERCADORIA</proPred><infQ><qCarga>1</qCarga></infQ></infCarga>'
             . '<infDoc>' . $infNFe . '</infDoc><infModal versaoModal="4.00"><rodo><RNTRC>' . htmlspecialchars($rntrc, ENT_XML1) . '</RNTRC></rodo></infModal></infCTeNorm>'
             . '</infCte></CTe>';
     }
