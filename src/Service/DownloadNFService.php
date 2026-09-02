07bd1fe5a6ad74afd1c63ca7f57b3c9af2936aa7
Luiz Kim <luiz.kim@controleonline.com>
2026-09-01 23:46:09 -0300

    fix(pdf): use latest DANFSe renderer when available

--- src/Service/DownloadNFService.php
+++ src/Service/DownloadNFService.php
@@ -7,6 +7,7 @@ use ControleOnline\Entity\People;
 use NFePHP\DA\CTe\Dacte;
 use NFePHP\DA\NFe\Danfce;
 use NFePHP\DA\NFe\Danfe;
+use NFePHP\DA\NFSe\Danfse;
 use NFePHP\POS\DanfcePos;
 use NFePHP\POS\PrintConnectors\Base64PrintConnector;
 use Symfony\Component\HttpFoundation\HeaderUtils;
@@ -125,6 +126,10 @@ class DownloadNFService
                 return (new Danfce($xml))->render($logo);
             }
 
+            if ($model === 0 && stripos($xml, '<CompNfse') !== false) {
+                return (new Danfse($xml))->render($logo);
+            }
+
             $danfe = new Danfe($xml);
             return $danfe->render($logo);
         } catch (\Throwable $legacy) {
@@ -135,6 +140,10 @@ class DownloadNFService
 
             if ($model === 65) {
                 return (new Danfce($xml))->render($logo);
+            }
+
+            if ($model === 0 && stripos($xml, '<CompNfse') !== false) {
+                return (new Danfse($xml))->render($logo);
             }
 
             $danfe = new Danfe($xml, 'P', 'A4', $logo, 'I', '');
